e40d567604a4e0a593cd2b620de09ac6e755f58d
Marina Hany <58513520+marinahanyy@users.noreply.github.com>
2020-12-25 17:47:29 +0200

    Add files via upload

---
+++ lab1.js
@@ -0,0 +1,16 @@
+var myImage=document.querySelector('img');
+
+myImage.onclick=function(){
+var mySrc=myImage.getAttribute('src');
+
+if (mySrc==='images/firefox-icon.png'){
+    
+myImage.setAttribute('src','images/rabbit.jpg');
+}
+else 
+
+{myImage.setAttribute('src','images/firefox-icon.png');
+
+
+}
+}
